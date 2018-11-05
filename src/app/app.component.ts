9b102b582885f435cf42484b9e9f6e1df694c84f
Yassine benzineb <ybenzineb@sqli.com>
2018-11-05 15:51:15 +0100

    commit project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'post-app';
-}
+
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Paragraph development begins with the formulation of the controlling idea. This idea directs the paragraph’s development. Often, the controlling idea of a paragraph will appear in the form of a topic sentence. In some cases, you may need more than one sentence to express a paragraph’s controlling idea.',
+      loveIts: 5,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Paragraph development continues with an expression of the rationale or the explanation that the writer gives for how the reader should interpret the information presented in the idea statement or topic sentence of the paragraph. The writer explains his/her thinking about the main topic, idea, or focus of the paragraph.',
+      loveIts: -2,
+      created_at: new Date()
+    },
+    {
+      title: 'Encore un post',
+      content: 'Paragraph development progresses with the expression of some type of support or evidence for the idea and the explanation that came before it. The example serves as a sign or representation of the relationship established in the idea and explanation portions of the paragraph.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+  ];
+
+
+  onLike(post) {
+     post.loveIts++;
+    }
+
+    onDislike (post) {
+     post.loveIts--;
+    }
+ }
---
+++ src/app/post-app/post-app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-app',
+  templateUrl: './post-app.component.html',
+  styleUrls: ['./post-app.component.scss']
+})
+export class PostAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
